d59853cec8235aad95b41cc8e59b8a806bd148db
Joshua <j.blengino@yahoo.com>
2022-03-16 13:19:42 -0700

    winning combinations added

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ class TicTacToe {
         this.board = board
     }
 
-    
+    get winningCombinations() {
+        return [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5 ,8],
+        ]
+    }
 }
 
 let game1 = new TicTacToe()
 console.log('GAME STARTED')
-console.log(game1)
+console.log(game1.board)
+console.log(game1.winningCombinations)
